09718600f807518d52fdeedc8de2f3d3ffc90d79
EmanueleDeCicco97 <emanueledecicco97@gmail.com>
2025-03-01 18:19:49 +0100

    fix 2

--- src/main/java/com/example/ProgettoPersona/controller/PersonaController.java
+++ src/main/java/com/example/ProgettoPersona/controller/PersonaController.java
@@ -17,35 +17,30 @@ public class PersonaController {
     @Autowired
     private PersonaService personaService;
 
-    // Get all Persone
     @GetMapping
     public ResponseEntity<List<Persona>> getAllPersone() {
         List<Persona> persone = personaService.getAll();
         return ResponseEntity.ok(persone);
     }
 
-    // Get Persona by ID
     @GetMapping("/{id}")
     public ResponseEntity<Persona> getPersonaById(@PathVariable int id) {
         Persona persona = personaService.getById(id);
         return ResponseEntity.ok(persona);
     }
 
-    // Create a new Persona
     @PostMapping
     public ResponseEntity<Persona> createPersona(@RequestBody Persona persona) {
         Persona createdPersona = personaService.create(persona);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdPersona);
     }
 
-    // Update an existing Persona
     @PutMapping("/{id}")
     public ResponseEntity<Persona> updatePersona(@PathVariable int id, @RequestBody Persona persona) {
         Persona updatedPersona = personaService.update(id, persona);
         return ResponseEntity.ok(updatedPersona);
     }
 
-    // Delete a Persona by ID
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePersona(@PathVariable int id) {
         personaService.delete(id);
